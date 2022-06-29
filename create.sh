9802c43b4a8ae15db3b27586213c8b78afcb2df9
Ajithkolal <93968708+Ajithkolal@users.noreply.github.com>
2022-06-29 18:46:58 +0530

    Update create.sh

--- create.sh
+++ create.sh
@@ -1,12 +1,15 @@
- #!/bin/bash
-      az login --tenant af5a6fb3-b792-4f0e-aa58-7cf18b9cad30
-      az account show --output yaml
-      az group create --name mytestrg --location eastasia
-      az network vnet create -g mytestrg -n testvnet --address-prefix 10.0.0.0/16 \
-                --subnet-name testsubnet --subnet-prefix 10.0.0.0/24
-      az network public-ip create -g mytestrg -n testip --allocation-method Dynamic
-      az network nsg create -g mytestrg -n testnsg
-      az network nic create -g mytestrg --vnet-name testvnet --subnet testsubnet -n testnic
-      az network nic ip-config create -g mytestrg -n testipConfig --nic-name testnic --make-primary
-      az vm create -n testvm -g mytestrg --image UbuntuLTS --public-ip-address testip --authentication-type password --admin-username demouser --admin-password Ajith@123456789
+#!/bin/bash
+echo "Execute your super awesome commands here!"
+mkdir -p /home/azureuser/myagent/ /home/azureuser/Downloads 
+cd /home/azureuser/Downloads
+wget https://vstsagentpackage.azureedge.net/agent/2.204.0/vsts-agent-linux-x64-2.204.0.tar.gz
+cd /home/azureuser/myagent/
+tar zxvf /home/azureuser/Downloads/vsts-agent-linux-x64-2.204.0.tar.gz
+sudo chmod -R 777 /home/azureuser/myagent/
+runuser -l azureuser -c '/home/azureuser/myagent/config.sh --unattended  --url https://dev.azure.com/aonkarappa --auth pat --token vjqo5xv3vqgqp7hayk2y2phbsiwjgbfu6srxdv6yoltf5oqpbrkq --pool ajithagent'
+sudo /home/azureuser/myagent/svc.sh install
+sudo /home/azureuser/myagent/svc.sh start
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+exit 0
+
 
