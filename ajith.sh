efe75611e82e2f7810e9498791e6a4b753d43ab8
Ajithkolal <93968708+Ajithkolal@users.noreply.github.com>
2022-06-30 14:28:52 +0530

    Update ajith.sh

--- ajith.sh
+++ ajith.sh
@@ -1,12 +1,11 @@
  #!/bin/bash
-      az login --tenant af5a6fb3-b792-4f0e-aa58-7cf18b9cad30
+      az login --tenant $(SUB_ID)
       az account show --output yaml
-      az group create --name $(RG) --location eastasia
+      az group create --name $(RG) --location $(LOCATION)
       az network vnet create -g $(RG) -n $(VNET) --address-prefix 10.0.0.0/16 \
-                --subnet-name testsubnet --subnet-prefix 10.0.0.0/24
-      az network public-ip create -g $(RG) -n testip --allocation-method Dynamic
-      az network nsg create -g $(RG) -n testnsg
-      az network nic create -g $(RG) --vnet-name $(VNET) --subnet testsubnet -n testnic
-      az network nic ip-config create -g $(RG) -n testipConfig --nic-name testnic --make-primary
-      az vm create -n testvm -g $(RG) --image UbuntuLTS --public-ip-address testip --authentication-type password --admin-username demouser --admin-password Ajith@123456789
-
+                --subnet-name $(SUBNET) --subnet-prefix 10.0.0.0/24
+      az network public-ip create -g $(RG) -n $(IP) --allocation-method Dynamic
+      az network nsg create -g $(RG) -n $(NSG)
+      az network nic create -g $(RG) --vnet-name $(VNET) --subnet $(SUBNET) -n $(NIC)
+      az network nic ip-config create -g $(RG) -n testipConfig --nic-name $(NIC) --make-primary
+      az vm create -n $(VM) -g $(RG) --image UbuntuLTS --public-ip-address $(IP) --authentication-type password --admin-username $(USER) --admin-password $(PASSWORD)
